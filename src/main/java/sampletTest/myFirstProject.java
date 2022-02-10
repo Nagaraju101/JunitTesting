226a84842414273767b398e2622170175f75e2a4
Nagaraju101 <Nagaraju101@gmail.com>
2022-02-09 21:19:40 -0700

    new files added

---
+++ src/main/java/sampletTest/myFirstProject.java
@@ -0,0 +1,15 @@
+package sampletTest;
+
+public class myFirstProject {
+
+	public static void main(String[] args) {
+		
+		String s1 = "Nagaraju";
+		String s2 = "Ankamreddi";
+		
+		System.out.println(s1+ " " + s2);
+
+	}
+
+}
+;
